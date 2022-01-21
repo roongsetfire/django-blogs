37951a39c8990f069589d2849ab122b95d1ac831
Roong <roongsetfire@gmail.com>
2022-01-21 16:02:41 +0700

    second commit

--- loginsystem/views.py
+++ loginsystem/views.py
@@ -1,3 +1,4 @@
+from tabnanny import check
 from django.shortcuts import redirect, render
 from django.contrib import messages
 from django.contrib.auth.models import User,auth
@@ -6,7 +7,8 @@ from writerpanel.views import panel
 
 # Create your views here.
 def index(request):
-    return render(request,'backend/login_register.html')
+    checkLogin = True
+    return render(request,'backend/login_register.html',{'checkLogin':checkLogin})
 
 def register(request):
     if request.method == 'POST':
@@ -14,27 +16,29 @@ def register(request):
         email = request.POST['email']
         password = request.POST['password']
         repassword = request.POST['repassword']
+        checkLogin = False
 
         if username == '' or email == '' or password == '' or repassword == '':
             messages.info(request,'please enter all the field')
-            return redirect('member')
+            return render(request,'backend/login_register.html',{'checkLogin':checkLogin})
         else:
             if password == repassword:
                 if User.objects.filter(username=username).exists():
                     messages.info(request,'Username is already in use')
-                    return redirect('member')
+                    return render(request,'backend/login_register.html',{'checkLogin':checkLogin})
                 elif User.objects.filter(email=email).exists():
                     messages.info(request,'email is already in use')
-                    return redirect('member')
+                    return render(request,'backend/login_register.html',{'checkLogin':checkLogin})
 
                 else:
                     user = User.objects.create_user(username=username,email=email,password=password)
                     user.save()
                     messages.info(request,'Register successful')
-                    return redirect('member')
+                    checkLogin = True
+                    return render(request,'backend/login_register.html',{'checkLogin':checkLogin})
             else:
                 messages.info(request,'password do not match. Please try again')
-                return redirect('member')
+                return render(request,'backend/login_register.html',{'checkLogin':checkLogin})
 
 def login(request):
     username = request.POST['username']
